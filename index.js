1fd76152a06d3d3b4afca33cb0b78b2bc740ca8c
Char Aznable <charazn37@gmail.com>
2016-12-30 21:41:57 +0800

    Add cool-ascii-faces module. Refactor code to standardjs syntax

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var app = express();
+var cool = require('cool-ascii-faces')
+var express = require('express')
+var app = express()
+var path = require('path')
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')))
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/', function (request, response) {
+  response.render('pages/index')
+})
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.get('/cool', function (request, response) {
+  response.send(cool())
+})
 
-
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
